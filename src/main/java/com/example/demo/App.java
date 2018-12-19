0094a49d18fa2ef59b3b37125cff3721f4630755
LAPTOP-0S3DR89Q\shenhanlin <1875768830@qq.com>
2018-12-19 13:57:16 +0800

    no message

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class App {
+	public static void main(String args[]){
+		System.out.println("hello world");
+		System.out.println("hello world2");
+		System.out.println("hello world3");
+		System.out.println("hello world4");
+	}
+}
